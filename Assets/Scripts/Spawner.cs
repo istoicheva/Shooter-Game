17cbd6d99ea9a92b7e07f78d7f21138c9c761de8
istoicheva <ina.stoicheva@gmail.com>
2017-07-22 14:56:22 +0300

    Initializing project

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public Wave[] waves;
+    public Enemy enemy;
+
+    Wave curruntWave;
+    int curruntWaveNumber;
+
+    int enemiesRemainingToSpawn;
+    int enemiesRemainingAlive;
+    float nextSpawnTime;
+
+    void Start() {
+        NextWave();
+    }
+
+    void Update() {
+        if (enemiesRemainingToSpawn > 0 && Time.time > nextSpawnTime) {
+            enemiesRemainingToSpawn--;
+            nextSpawnTime = Time.time + curruntWave.timeBetweenSpawns;
+
+            Enemy spawnedEnemy = Instantiate(enemy, Vector3.zero, Quaternion.identity) as Enemy;
+            spawnedEnemy.OnDeath += OnEnemyDeath;
+        }
+    }
+
+    void OnEnemyDeath() {
+        enemiesRemainingAlive--;
+
+        if (enemiesRemainingAlive == 0) {
+            NextWave();
+        }
+    }
+
+    void NextWave() {
+        curruntWaveNumber++;
+        if (curruntWaveNumber - 1 <waves.Length) {
+            curruntWave = waves[curruntWaveNumber - 1];
+        }
+
+        enemiesRemainingToSpawn = curruntWave.enemyCount;
+        enemiesRemainingAlive = enemiesRemainingToSpawn;
+    }
+
+    [System.Serializable]
+    public class Wave {
+        public int enemyCount;
+        public float timeBetweenSpawns;
+
+    }
+
+}
